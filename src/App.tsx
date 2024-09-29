2014c0b6b41dc46b57582390c3aa8d550a5646ec
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-29 16:43:23 -0300

    feat: HeaderComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { ShoppingCart } from "lucide-react"
+
 function App() {
 
   return (
     <>
-      <h1 className="bg-red-300">hello world</h1>      
+      <div className="h-32 flex justify-center items-center">
+        <nav className="flex items-center gap-5 text-xl font-normal text-zinc-500">
+          <div>CandyCrush</div>
+          <button>
+            <ShoppingCart />
+          </button>
+        </nav>  
+      </div>      
     </>
   )
 }
